e0ca00bb48aa5fb5742c7eb0d92071d36c910dd7
Abhay-Yadav1 <141158678+Abhay-Yadav1@users.noreply.github.com>
2026-03-14 23:47:47 +0530

    workflow done

--- agents/schema_workflow.py
+++ agents/schema_workflow.py
@@ -2,3 +2,13 @@ from langgraph.graph import StateGraph, START, END
 from graphs.state1 import SchemaInfo
 from agents.schema_selector import schema_selector_node
 
+def create_schema_workflow():
+    workflow=StateGraph(SchemaInfo)
+    workflow.add_node("select_tables", schema_selector_node)
+    workflow.add_edge(START, "select_tables")
+    workflow.add_edge("select_tables", END)
+    app=workflow.compile()
+    return app
+
+
+
